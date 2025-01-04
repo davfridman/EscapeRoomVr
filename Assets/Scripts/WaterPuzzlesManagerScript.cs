17742c65364740bd01aa993e090f3701a586306e
Segev Chen <segev.chen@mail.huji.ac.il>
2025-01-04 21:00:28 +0200

    Puzzle 1 works!!!
    We hate the fake kkk!!!

--- Assets/Scripts/WaterPuzzlesManagerScript.cs
+++ Assets/Scripts/WaterPuzzlesManagerScript.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaterPuzzlesManagerScript : MonoBehaviour
+public class GoofScript : MonoBehaviour
 {
     [SerializeField] private GameObject wallOpen;
-    [SerializeField] private bool socketA = false;
-    [SerializeField] private bool socketB = false;
+    [SerializeField] private int numberOfSockets = 4;
+    private bool[] sockets;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sockets = new bool[numberOfSockets];
+        for (int i = 0; i < numberOfSockets; i++)
+        {
+            sockets[i] = false;
+        }
     }
 
     // Update is called once per frame
@@ -19,24 +23,71 @@ public class WaterPuzzlesManagerScript : MonoBehaviour
         
     }
 
-    public void SockPlaced()
+    public void SockPlaced(GameObject socket)
     {
-        socketA = true;
-        OpenWall();
+        // Get the tag
+        string tag = socket.tag;
+
+        // Extract the last character of the tag and convert it to an integer
+        int sockNum = GetSockNum(tag);
+        int sockIndex = sockNum - 1;
+        //Debug.Log("sockNum: " + sockIndex);
+
+        // Check if socket is legal
+        if (!CheckSocketIndexValid(sockIndex)) {return;}
+
+        // Update socket status
+        sockets[sockIndex] = true;
+        CheckAllSockets();
     }
 
-    public void SockUnplaced() { socketA = false; }
-
-    public void SockBPlaced()
+    public int GetSockNum(string sockTag)
     {
-        socketB = true;
-        OpenWall();
+        // Extract the last character of the tag and convert it to an integer
+        if (!string.IsNullOrEmpty(sockTag) && char.IsDigit(sockTag[sockTag.Length - 1]))
+        {
+            int sockTagNum = int.Parse(sockTag[sockTag.Length - 1].ToString());
+            //Debug.Log("Last character of tag as int: " + sockTagNum);
+            return sockTagNum;
+        }
+        Debug.LogWarning("Tag is empty or does not end with a digit.");
+        return 0;
     }
-    public void SockBUnplaced() { socketB = false; }
 
-    private void OpenWall()
+    public bool CheckSocketIndexValid(int sockIndex)
+    {
+        if (sockIndex == -1) {return false;}
+        if (sockIndex >= numberOfSockets) {return false;}
+        return true;
+    }
+
+    public void SockUnplaced(GameObject socket) 
+    { 
+        // Get the tag
+        string tag = socket.tag;
+
+        // Extract the last character of the tag and convert it to an integer
+        int sockNum = GetSockNum(tag);
+        int sockIndex = sockNum - 1;
+        //Debug.Log("sockNum: " + sockIndex);
+
+        // Check if socket is legal
+        if (!CheckSocketIndexValid(sockIndex)) {return;}
+
+        // Update socket status
+        sockets[sockIndex] = false;
+
+    }
+
+    private void CheckAllSockets()
     {
         if (wallOpen == null) { return; }
-        if (socketA && socketB) { Destroy(wallOpen); }
+
+        // Check if all sockets are true
+        for (int i = 0; i < numberOfSockets; i++)
+        {
+            if (!sockets[i]) { return; }
+        }
+        Destroy(wallOpen);
     }
 }
